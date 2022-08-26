2e7d779259c719f31c5ccb5660998d8e6781751b
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-08-26 22:02:53 +0530

    feat: created progress bar

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,36 @@
+export const ProgressBar = ({ setSteps, step }) => {
+  return (
+    <div className="progress-bar flex">
+      {Array.from({ length: 4 }).map((item, index) => {
+        return (
+          <div key={index} className="relative flex items-center">
+            <div
+              className={`h-8 w-8 rounded-full flex items-center cursor-pointer
+                 justify-center text-white ${
+                   index + 1 <= step
+                     ? "primary-bg-color"
+                     : "bg-gray-50 border text-black"
+                 } `}
+              onClick={() => setSteps(index + 1)}
+            >
+              {index + 1}
+            </div>
+            {index < 3 && ( //to limit progress line till 4th step
+              <div className="md:w-20 w-14 bg-gray-200 h-[2px] transition">
+                <div
+                  className={`primary-bg-color h-[2px] ${
+                    index + 1 < step
+                      ? "w-full"
+                      : index + 1 === step
+                      ? "w-1/2"
+                      : "w-0"
+                  }`}
+                ></div>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
